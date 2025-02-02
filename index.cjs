3254371ecfb301ab72ce53078eebfff1547e4890
Nahum Alon <nochum.shepsl@gmail.com>
2025-02-02 12:43:42 +0200

    modules (cjs)

--- index.cjs
+++ index.cjs
@@ -1,8 +1,4 @@
 const { logger } = require("./logger.cjs");
-// console.log(logger);
-// logger.log("Hello");
-
-//console.log(process);
 
 const path = require("node:path");
 logger.log(path.parse(__filename));
