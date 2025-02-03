3b7eca00432c8fc7aa672c3d0e5ae64edc7849ab
Nahum Alon <nochum.shepsl@gmail.com>
2025-02-03 11:30:11 +0200

    async filesystem (callback and promises )

--- index.mjs
+++ index.mjs
@@ -1,6 +1,19 @@
 import logger from "./logger.mjs";
-import fs from "node:fs";
+//import fs from "node:fs";
+import {readFile, writeFile} from "node:fs/promises";
 
-const data = fs.readFileSync("./index.mjs");
-logger.info(data);
-fs.writeFileSync("./file.txt", ["some data1", "some data2"].join("\n"));
+// const data = fs.readFile("./file.txt", "utf-16le",(err, data) => {
+//     if (err) {
+//         logger.error(err);
+//     } else {
+//         logger.info(data);
+//     }
+// });
+
+// fs.writeFile("./file.txt", ["some data1", "some data2"].join("\n"), "utf-16le", () => logger.info("file saved successfully"));
+
+(async () => {
+    const data = await readFile("./file.txt", "utf-16le");
+    logger.info(data)
+    writeFile("./file.txt", ["some data1", "some data2"].join("\n"), "utf-16le");
+})();
